4bdc9a65688d5cdce80952bfe453e65520682b61
Dominik Mathmann <dominik.mathmann@gedoplan.de>
2016-12-23 13:54:19 +0100

    Swagger Demo Client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { DefaultApi } from './gen/api/api'
+import { CurrentTime } from './gen/model/models'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DefaultApi]
 })
 export class AppComponent {
-  title = 'app works!';
+
+  constructor(private _api: DefaultApi) {
+
+  }
+
+  public echo: string;
+  public time: CurrentTime;
+
+  public getEcho(name: string, message: string) {
+    this._api.echoNameGet(name, message).subscribe( (resp:any) => this.echo=resp.message);
+  }
+
+  public getTime() {
+    this._api.timeGet().subscribe(t => this.time=t)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {BASE_PATH} from './gen/variables'
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    {provide: BASE_PATH, useValue: "http://localhost:8080"}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
